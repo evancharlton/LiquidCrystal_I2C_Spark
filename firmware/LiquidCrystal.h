8f9c274408a4a7a5d29d10a7bf26f1e3db12c0d7
Evan Charlton <evancharlton@gmail.com>
2015-05-16 14:40:38 -0700

    Split into library

--- firmware/LiquidCrystal.h
+++ firmware/LiquidCrystal.h
@@ -1,72 +1,9 @@
-//-----------------------------------------------//
-// I2C / SPI LCD BACKPACK LIBRARY FOR SPARK CORE //
-//===============================================//
-// Copy this into a new application at:          //
-// https://www.spark.io/build and go nuts!       //
-// !! Pinouts on line 427 below !!               //
-//-----------------------------------------------//
-// Technobly / BDub - Jan 16th 2014              //
-//===============================================//
+#include "application.h"
 
-// http://www.adafruit.com/products/292
-// https://github.com/adafruit/LiquidCrystal
+#ifndef LIQUIDCRYSTAL_H
+#define LIQUIDCRYSTAL_H
 
-/* ===================== Adafruit_MCP23008.h ========================= */
-
-#define _BV(bit) (1 << (bit)) // used in LiquidCrystal.cpp
-
-/*************************************************** 
-  This is a library for the MCP23008 i2c port expander
-
-  These displays use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-// Don't forget the Wire library
-class Adafruit_MCP23008 {
-public:
-  void begin(uint8_t addr);
-  void begin(void);
-
-  void pinModeMCP(uint8_t p, uint8_t d);
-  void digitalWrite(uint8_t p, uint8_t d);
-  void pullUp(uint8_t p, uint8_t d);
-  uint8_t digitalRead(uint8_t p);
-  uint8_t readGPIO(void);
-  void writeGPIO(uint8_t);
-
- private:
-  uint8_t i2caddr;
-  uint8_t read8(uint8_t addr);
-  void write8(uint8_t addr, uint8_t data);
-};
-
-// Default address is 0x20 - 0x27
-#define MCP23008_ADDRESS 0x20
-
-// registers
-#define MCP23008_IODIR 0x00
-#define MCP23008_IPOL 0x01
-#define MCP23008_GPINTEN 0x02
-#define MCP23008_DEFVAL 0x03
-#define MCP23008_INTCON 0x04
-#define MCP23008_IOCON 0x05
-#define MCP23008_GPPU 0x06
-#define MCP23008_INTF 0x07
-#define MCP23008_INTCAP 0x08
-#define MCP23008_GPIO 0x09
-#define MCP23008_OLAT 0x0A
-
-
-/* ===================== LiquidCrystal.h ========================= */
-
-//#include "Print.h"
+#define _BV(bit) (1 << (bit))
 
 // commands
 #define LCD_CLEARDISPLAY 0x01
@@ -178,4 +115,6 @@ private:
   uint8_t _i2cAddr;
   Adafruit_MCP23008 _i2c;
 };
+
+#endif // LIQUIDCRYSTAL_H
 
